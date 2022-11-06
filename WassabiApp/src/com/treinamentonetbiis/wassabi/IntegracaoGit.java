b3988b01b3f60104bab139678b356a41d8aa3ab7
Matheus2gps <83222686+Matheus2gps@users.noreply.github.com>
2022-11-06 13:25:35 -0300

    Arquivos adicionados via upload

---
+++ WassabiApp/src/com/treinamentonetbiis/wassabi/IntegracaoGit.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.treinamentonetbiis.wassabi;
+
+/**
+ * @author Matheus Gonçalves
+ * 
+ * Este arquivo implementa IntegracaoGit
+ * Implementado em 06-11-2022
+ */
+public class IntegracaoGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Teste!");
+
+	}
+
+}
